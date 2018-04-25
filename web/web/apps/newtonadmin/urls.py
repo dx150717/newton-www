8f7fe1fa28273c91d2c5660c1ebbcf444be2f88f
weixuefeng <weixuefeng@lubangame.com>
2018-04-25 16:43:11 +0800

    debug

--- web/web/apps/newtonadmin/urls.py
+++ web/web/apps/newtonadmin/urls.py
@@ -4,14 +4,14 @@ from . import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                    url(r'^$', views.index, name='newtonAdmin'),
+                    url(r'^$', views.index),
                     url(r'^login/$', views.show_login_view),
                     url(r'^login/post/$', views.post_login),
                     url(r'^logout/$', views.logout),
-                    url(r'^kyc/$', views.kyc_admin, name='kyc_admin'),
-                    url(r'^kyc/detail/', views.kyc_detail, name='kyc_detail'),
-                    url(r'^kyc/update/', views.kyc_update, name='update_kycinfo'),
+                    url(r'^kyc/$', views.kyc_admin),
+                    url(r'^kyc/detail/', views.kyc_detail),
+                    url(r'^kyc/update/', views.kyc_update),
                     url(r'^kyc/send-email/', views.kyc_send_email),
                     url(r'^kyc/export-csv/', views.kyc_export_csv),
-                    url(r'^blog/$', views.blog_admin, name='blog_admin'),
+                    url(r'^blog/$', views.blog_admin),
 )
