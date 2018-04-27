ca8757b0578dfbeb962bf37c4652404303c9a4d2
Xia Wu <xiawu@newtonproject.org>
2018-04-27 12:41:32 +0800

    add the interface definition

---
+++ web/web/apps/verification/services.py
@@ -0,0 +1,26 @@
+"""Service Implementation of verification module
+
+"""
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def generate_verification_uuid(email):
+    """
+    """
+    try:
+        # genreate uuid
+        # save db
+        # return result
+    except Exception, inst:
+        return None
+
+def get_verification_by_uuid(uuid):
+    """
+    """
+    try:
+        # query
+        # return result
+    except Exception, inst:
+        return None
