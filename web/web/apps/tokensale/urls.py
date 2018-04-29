82cc320bfb8daea901b36deab4bdd9933c6f8715
Xia Wu <xiawu@newtonproject.org>
2018-04-29 18:23:57 +0800

    make the last changes work

--- web/web/apps/tokensale/urls.py
+++ web/web/apps/tokensale/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.show_kyc_index_view),
-    url(r'^join/$', views.show_join_kyc_view),
+	url(r'^$', views.show_tokensale_index_view),
+        url(r'^join/$', views.show_join_tokensale_view),
 	url(r'^post/$', views.post_kyc_information),
 	url(r'^wait-audit/$', views.show_wait_audit_view),
 )
--- web/web/apps/tokensale/views.py
+++ web/web/apps/tokensale/views.py
@@ -15,30 +15,30 @@ from . import services
 
 logger = logging.getLogger(__name__)
 
-def show_kyc_index_view(request):
-    return render(request, "kyc/index.html", locals()) 
+def show_tokensale_index_view(request):
+    return render(request, "tokensale/index.html", locals()) 
 
-def show_join_kyc_view(request):
+def show_join_tokensale_view(request):
     form = forms.KYCInfoForm()
-    return render(request, "kyc/submit.html", locals()) 
+    return render(request, "tokensale/submit.html", locals()) 
 
 @login_required
 @decorators.http_post_required
 def post_kyc_information(request):
-	try:
-		# TODO twice submit kyc info need check.
-		form = forms.KYCInfoForm(request.POST)
-		if not form.is_valid():
-			return render(request, "kyc/submit.html", locals()) 
-		instance = form.save(commit=False)
-		instance.user = request.user
-		instance.save()
-		email = request.user.email
-		services.send_kyc_confirm_email(email, request)
-		return redirect('/kyc/wait-audit/')
-	except Exception, inst:
-		logger.exception("fail to post kyc information:%s" % str(inst))
-		return http.HttpResponseServerError()
+    try:
+        # TODO twice submit tokensale info need check.
+        form = forms.KYCInfoForm(request.POST)
+        if not form.is_valid():
+            return render(request, "tokensale/submit.html", locals())
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        email = request.user.email
+        services.send_kyc_confirm_email(email, request)
+        return redirect('/tokensale/wait-audit/')
+    except Exception, inst:
+        logger.exception("fail to post kyc information:%s" % str(inst))
+        return http.HttpResponseServerError()
 
 def show_wait_audit_view(request):
-    return render(request, "kyc/wait-audit.html", locals())
+    return render(request, "tokensale/wait-audit.html", locals())
