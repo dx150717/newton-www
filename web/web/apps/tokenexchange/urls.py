dccc7f031805394018bc6d8020efb33465e5f0bb
Xia Wu <xiawu@newtonproject.org>
2018-05-05 16:30:14 +0800

    fix bug

--- web/web/apps/tokenexchange/urls.py
+++ web/web/apps/tokenexchange/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
                        url(r'^wait-audit/$', views.show_wait_audit_view),
                        url(r'^pending/', views.show_pending_view),
                        url(r'^end/', views.show_end_view),
-                       url(r'^invite/(?P<invite_id>[0-9]+)/$', views.post_apply_amount),
+                       url(r'^invite/(?P<invite_id>[0-9]+)/post/$', views.post_apply_amount),
                        
                        url(r'^invalid-link/$', views.show_invalid_link),
                        url(r'^(?P<username>[0-9a-z]+)/$', views.show_receive_address_view),
