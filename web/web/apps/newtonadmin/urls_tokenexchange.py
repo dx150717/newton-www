e15970af4f61f9650ca5cdcde9285d6ce7c231f1
Xia Wu <xiawu@newtonproject.org>
2018-05-05 10:57:56 +0800

    add the te wait list page

--- web/web/apps/newtonadmin/urls_tokenexchange.py
+++ web/web/apps/newtonadmin/urls_tokenexchange.py
@@ -6,17 +6,15 @@ from . import views_tokenexchange
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                    url(r'^$', views_tokenexchange.show_id_list_view),
-                    url(r'^id/$', views_tokenexchange.show_id_list_view),
-                    url(r'^id/confirm/', views_tokenexchange.confirm_id),
-
-                    url(r'^amount/$', views_tokenexchange.show_amount_list_view),
-                    url(r'^amount/confirm/', views_tokenexchange.confirm_amount),
-
-                    url(r'^email/$', views_tokenexchange.show_email_list_view),
-                    url(r'^email/confirm/', views_tokenexchange.confirm_email),
-
-                    url(r'^sent/$', views_tokenexchange.show_sent_list_view),                    
-
-                    url(r'^receive/$', views_tokenexchange.show_receive_list_view),                    
+                       url(r'^$', views_tokenexchange.show_id_list_view),
+                       url(r'^id/$', views_tokenexchange.show_id_list_view),
+                       url(r'^id/pass/', views_tokenexchange.show_pass_id_list_view),
+                       url(r'^id/confirm/', views_tokenexchange.confirm_id),
+                       url(r'^invite/(?P<phase_id>[0-9]+)/', views_tokenexchange.show_invite_view),
+                       url(r'^amount/$', views_tokenexchange.show_amount_list_view),
+                       url(r'^amount/confirm/', views_tokenexchange.confirm_amount),
+                       url(r'^email/$', views_tokenexchange.show_email_list_view),
+                       url(r'^email/confirm/', views_tokenexchange.confirm_email),
+                       url(r'^sent/$', views_tokenexchange.show_sent_list_view),
+                       url(r'^receive/$', views_tokenexchange.show_receive_list_view),                    
 )
