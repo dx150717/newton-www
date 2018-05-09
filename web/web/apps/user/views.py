868f5d3b380dfb09719b51091e1fb2b6de1b0025
weixuefeng <weixuefeng@lubangame.com>
2018-05-09 14:23:32 +0800

    change progress for invite investor

--- web/web/apps/user/views.py
+++ web/web/apps/user/views.py
@@ -24,7 +24,7 @@ def show_user_index_view(request):
     form = forms.UserForm(instance=user)
     kycinfo = tokenexchange_models.KYCInfo.objects.filter(user=user).first()
     kycaudit = tokenexchange_models.KYCAudit.objects.filter(user=user).first()
-    items = tokenexchange_models.InvestInvite.objects.filter(user=user, status=codes.TOKEN_EXCHANGE_STATUS_SEND_INVITE_NOTIFY_VALUE)
+    items = tokenexchange_models.InvestInvite.objects.filter(user=user,status__gte=codes.TOKEN_EXCHANGE_STATUS_SEND_INVITE_NOTIFY_VALUE)
     for item in items:
         item.token_exchange_info = settings.FUND_CONFIG[item.phase_id]
     return render(request, "user/index.html", locals())
@@ -81,7 +81,7 @@ def show_token_exchange_progress_view(request, phase_id):
         user = request.user
         kycinfo = tokenexchange_models.KYCInfo.objects.filter(user=user).first()
         kycaudit = tokenexchange_models.KYCAudit.objects.filter(user=user).first()
-        item = tokenexchange_models.InvestInvite.objects.filter(user=user, phase_id=phase_id).first()
+        item = tokenexchange_models.InvestInvite.objects.filter(user=user, phase_id=phase_id, status__gte=codes.TOKEN_EXCHANGE_STATUS_SEND_INVITE_NOTIFY_VALUE).first()
         if item:
             token_exchange_info = settings.FUND_CONFIG[item.phase_id]
             btc_address = item.receive_btc_address
