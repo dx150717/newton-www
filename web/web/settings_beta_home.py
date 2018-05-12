5316ff4fa0e3804c0e606685c9c1f5b225712f1e
Xia Wu <xiawu@newtonproject.org>
2018-05-12 19:55:38 +0800

    fix ticket #92

--- web/web/settings_beta_home.py
+++ web/web/settings_beta_home.py
@@ -3,7 +3,6 @@ from settings_beta import *
 STATIC_URL = 'http://home.newtonproject.beta.diynova.com/static/'
 DOMAIN = 'home.newtonproject.beta.diynova.com'
 BASE_URL = 'http://home.newtonproject.beta.diynova.com'
-SESSION_COOKIE_DOMAIN = None
 MEDIA_URL = 'http://home.newtonproject.beta.diynova.com/filestorage/'
 
 ROOT_URLCONF = 'web.urls_home'
