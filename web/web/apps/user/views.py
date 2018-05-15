3c130a1588bd65be4f04d5293230308ef2c33848
weixuefeng <weixuefeng@lubangame.com>
2018-05-15 21:07:58 +0800

    adjust code

--- web/web/apps/user/views.py
+++ web/web/apps/user/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.db.models import Q, F
+from django.db.models import Sum
 from django.contrib.auth.models import User
 from django.conf import settings
 import pyotp
@@ -21,8 +22,11 @@ logger = logging.getLogger(__name__)
 
 @login_required
 def show_user_index_view(request):
+    """
+    Show user index view.includes basic information, kyc information, tokenexchange information.
+    """
     user = request.user
-    form = forms.UserForm(instance=user)
+    user_form = forms.UserForm(instance=user)
     kycinfo = tokenexchange_models.KYCInfo.objects.filter(user_id=user.id).first()
     if kycinfo:
         data = {}
@@ -47,53 +51,10 @@ def show_user_index_view(request):
     return render(request, "user/index.html", locals())
 
 @login_required
-@decorators.http_get_required
-def show_user_profile_view(request):
-    profile = models.UserProfile.objects.filter(user=request.user).first()
-    form = forms.UserProfileForm(instance=profile)
-    return render(request, "user/profile.html", locals())
-
-@login_required
-@decorators.http_post_required
-def post_profile(request):
-    try:
-        profile = models.UserProfile.objects.filter(user=request.user).first()
-        form = forms.UserProfileForm(request.POST,instance=profile)
-        if not form.is_valid():
-            return render(request, "user/profile.html", locals())
-        form.save()
-        cellphone_group = form.cleaned_data['cellphone_group']
-        country_code = cellphone_group[0]
-        cellphone = cellphone_group[1]
-        profile.country_code = country_code
-        profile.cellphone = cellphone
-        profile.save()
-        return http.HttpResponseRedirect("/user/")
-    except Exception, inst:
-        logger.exception("fail to post profile %s" %str(inst))
-        return http.HttpResponseServerError()
-
-@login_required
-def show_settings_view(request):
-    profile = models.UserProfile.objects.filter(user=request.user).first()
-    if not profile:
-        return http.HttpResponseRedirect("/user/profile/")
-    if profile.is_google_authenticator:
-        toggle = "true"
-    else:
-        toggle = "false"
-    return render(request, "user/settings.html", locals())
-
-@login_required
-def get_qrcode(request):
-    gtoken = pyotp.random_base32()
-    uri = pyotp.totp.TOTP(gtoken).provisioning_uri("newton",issuer_name="newton")
-    return http.JsonSuccessResponse(data={"gtoken":gtoken,"uri":uri})
-
-def post_settings(request):
-    return render(request, "user/settings.html", locals())
-
 def show_token_exchange_progress_view(request, phase_id):
+    """
+    query user who pass the kyc, than render his progress information.
+    """
     try:
         user = request.user
         kycinfo = tokenexchange_models.KYCInfo.objects.filter(user_id=user.id).first()
@@ -104,15 +65,9 @@ def show_token_exchange_progress_view(request, phase_id):
         if item:
             token_exchange_info = settings.FUND_CONFIG[item.phase_id]
             if item.receive_btc_address:
-                btc_transaction = tokenexchange_models.AddressTransaction.objects.filter(address=item.receive_btc_address,address_type=codes.CurrencyType.BTC.value)
-                if btc_transaction:
-                    for t in btc_transaction:
-                        btc_final_balance = btc_final_balance + float(t.value)
+                btc_final_balance = tokenexchange_models.AddressTransaction.objects.filter(address=item.receive_btc_address,address_type=codes.CurrencyType.BTC.value).aggregate(Sum('value'))
             if item.receive_ela_address:
-                ela_transaction = tokenexchange_models.AddressTransaction.objects.filter(address=item.receive_ela_address,address_type=codes.CurrencyType.ELA.value)
-                if ela_transaction:
-                    for t in ela_transaction:
-                        ela_final_balance = ela_final_balance + float(t.value)
+                ela_final_balance = tokenexchange_models.AddressTransaction.objects.filter(address=item.receive_ela_address,address_type=codes.CurrencyType.ELA.value).aggregate(Sum('value'))
         if btc_final_balance != 0:
             item.btc_final_balance = btc_final_balance
         if ela_final_balance != 0:
