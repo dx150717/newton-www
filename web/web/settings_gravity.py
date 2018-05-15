9d4289f6e2f5ec8ba56df015d83dfb5623a4b4fc
Xia Wu <xiawu@newtonproject.org>
2018-05-15 15:36:02 +0800

    use https instead of http

--- web/web/settings_gravity.py
+++ web/web/settings_gravity.py
@@ -1,8 +1,8 @@
 from settings import *
 
-STATIC_URL = 'http://gravity.newtonproject.org/static/'
+STATIC_URL = 'https://gravity.newtonproject.org/static/'
 DOMAIN = 'gravity.newtonproject.org'
-BASE_URL = 'http://gravity.newtonproject.org'
-MEDIA_URL = 'http://gravity.newtonproject.org/filestorage/'
+BASE_URL = 'https://gravity.newtonproject.org'
+MEDIA_URL = 'https://gravity.newtonproject.org/filestorage/'
 
 ROOT_URLCONF = 'web.urls_gravity'
--- web/web/settings_home.py
+++ web/web/settings_home.py
@@ -1,8 +1,8 @@
 from settings_rc import *
 
-STATIC_URL = 'http://home.newtonproject.org/static/'
+STATIC_URL = 'https://home.newtonproject.org/static/'
 DOMAIN = 'home.newtonproject.org'
-BASE_URL = 'http://home.newtonproject.org'
-MEDIA_URL = 'http://home.newtonproject.org/filestorage/'
+BASE_URL = 'https://home.newtonproject.org'
+MEDIA_URL = 'https://home.newtonproject.org/filestorage/'
 
 ROOT_URLCONF = 'web.urls_home'
