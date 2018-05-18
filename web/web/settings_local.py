1504d3fe9b1b4788bb3d83aefb3a44cbeece9858
weixuefeng <weixuefeng@lubangame.com>
2018-05-18 23:16:14 +0800

    change version

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 151
+STATIC_DEFAULT_VERSION = 152
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
