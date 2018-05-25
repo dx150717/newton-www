38e6bdc86e7d2f985124eea3254237ee726d6eba
liuchao <cumtbliu@163.com>
2018-05-25 11:49:46 +0800

    add go top

--- web/web/static/js/index.js
+++ web/web/static/js/index.js
@@ -236,5 +236,22 @@ function getErrorMessage(json)
 }
 
 
-
+function gotoTop(min_height){
+    $("#gotoTop").click(
+        function(){$('html,body').animate({scrollTop:0},700);
+    }).hover(
+        function(){$(this).addClass("hover");},
+        function(){$(this).removeClass("hover");
+    });
+    min_height ? min_height = min_height : min_height = 800;
+    $(window).scroll(function(){
+        var s = $(window).scrollTop();
+        if( s > min_height){
+            $("#gotoTop").fadeIn(100);
+        }else{
+            $("#gotoTop").fadeOut(200);
+        };
+    });
+};
+gotoTop();
 
