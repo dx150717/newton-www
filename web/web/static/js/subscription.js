4e28c0f92c8e5e8c64d768dd5ac8f8564ed44f77
Xia Wu <xiawu@newtonproject.org>
2018-06-07 03:53:23 +0800

    fix bug

--- web/web/static/js/subscription.js
+++ web/web/static/js/subscription.js
@@ -14,7 +14,9 @@ $('#subscription_form').submit(function(event){
 	var email_address = $(form).find("input[type='text']").val();
 	if(email_address.length > 0) {
 		if(emailReg.test(email_address)) {
-			grecaptcha.reset();
+			if (grecaptcha) {
+				grecaptcha.reset();
+			}
 			$('.recaptcha-modal').modal();
 		}
 	}
