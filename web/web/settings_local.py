54d2748bdb95c702446f696aae7938496a48d673
liuchao <cumtbliu@163.com>
2018-06-07 21:39:28 +0800

    hide join us in mobile

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 190
+STATIC_DEFAULT_VERSION = 191
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
