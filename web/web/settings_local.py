885d3a8aa7559dd7835ae84f3195baa3771a3bb4
liuchao <cumtbliu@163.com>
2018-06-07 16:04:43 +0800

    change language icon

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 186
+STATIC_DEFAULT_VERSION = 187
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
