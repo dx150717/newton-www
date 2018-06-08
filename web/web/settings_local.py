abde377797065ddac08839aeecbe326fa4be9ef2
liuchao <cumtbliu@163.com>
2018-06-08 16:00:06 +0800

    change slogn

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 197
+STATIC_DEFAULT_VERSION = 198
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
