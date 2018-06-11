ddbed21e934d2b28b7cb7ef7482540a1037e0ea2
liuchao <cumtbliu@163.com>
2018-06-11 17:31:17 +0800

    update show kyc html

--- web/web/apps/tokenexchange/forms.py
+++ web/web/apps/tokenexchange/forms.py
@@ -7,21 +7,6 @@ from tokenexchange import models as tokenexchange_models
 from user import forms as user_forms
 
 '''
-first_name = models.CharField(max_length=128, verbose_name='First Name', required=True)
-last_name = models.CharField(max_length=128, verbose_name='Last Name', required=True)
-country_code = models.CharField(max_length=4, db_index=True, required=True)            
-cellphone = models.CharField(max_length=20, db_index=True, required=True)
-country = CountryField(blank_label="Select country or region", verbose_name='Country or Region', required=True)
-state = models.CharField(max_length=256, verbose_name='State', required=True)
-location = models.CharField(max_length=1024, verbose_name='Location', required=True)
-id_type = models.IntegerField(
-    _('id_type'),
-    choices=ID_CHOICES, default=codes.IDType.ID_CARD.value,
-    db_index=True,
-    required=True
-)
-id_number = models.CharField(max_length=128, db_index=True, verbose_name='ID Number', required=True)
-id_card = models.FileField(required=True, upload_to=storage.hashfile_upload_to('id_card', path_prefix='id_card'), verbose_name='ID Photo', validators=[validators.validate_file_extension_of_id_photo, validators.validate_file_size_of_id_photo])
 '''
 
 class KYCBaseForm(ModelForm):
@@ -60,8 +45,6 @@ class KYCProfileForm(ModelForm):
     """ kyc profile """
     def __init__(self, *args, **kw):
         super(KYCProfileForm, self).__init__(*args, **kw)
-        self.fields['personal_profile'].required = False
-        self.fields['personal_profile_attachment'].required = False
         self.fields['facebook'].required = False
         self.fields['twitter'].required = False
         self.fields['telegram'].required = False
@@ -95,6 +78,10 @@ class ContributeForm(ModelForm):
     """how to contribut for newton form"""
     def __init__(self, *args, **kw):
         super(ContributeForm, self).__init__(*args, **kw)
+        self.fields['what_is_newton'].required = False
+        self.fields['your_node_name'].required = False
+        self.fields['your_node_organizer'].required = False
+        self.fields['your_node_organizer_contact'].required = False
         self.fields.keyOrder = [
             'your_node_name',
             'your_node_organizer',
@@ -102,7 +89,7 @@ class ContributeForm(ModelForm):
             'done_for_newton',
             'done_for_newton_attachment',
             'do_for_newton',
-            'what_is_newton'
+            'what_is_newton',
         ]
         
     class Meta:
