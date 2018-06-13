2a331456f3763d69f5e8ac6bd2fe158d44f46578
weixuefeng <weixuefeng@lubangame.com>
2018-06-13 12:18:15 +0800

    add translate for kyc form

--- web/web/apps/tokenexchange/models.py
+++ web/web/apps/tokenexchange/models.py
@@ -28,38 +28,38 @@ class KYCInfo(models.Model):
 
     user_id = models.IntegerField()
     # base info
-    first_name = models.CharField(max_length=128, verbose_name='First Name', help_text='(*)')
-    last_name = models.CharField(max_length=128, verbose_name='Last Name', help_text='(*)')
+    first_name = models.CharField(max_length=128, verbose_name=_('First Name'), help_text='(*)')
+    last_name = models.CharField(max_length=128, verbose_name=_('Last Name'), help_text='(*)')
     country_code = models.CharField(max_length=4, db_index=True, help_text='(*)')            
     cellphone = models.CharField(max_length=20, db_index=True, verbose_name='Cellphone', help_text='(*)')
-    country = CountryField(blank_label="Select country or region", verbose_name='Country or Region', help_text='(*)')
-    city = models.CharField(max_length=256, verbose_name='City', help_text='(*)')
-    location = models.CharField(max_length=1024, verbose_name='Address', help_text='(*)')
+    country = CountryField(blank_label=_("Select country or region"), verbose_name=_('Country or Region'), help_text='(*)')
+    city = models.CharField(max_length=256, verbose_name=_('City'), help_text='(*)')
+    location = models.CharField(max_length=1024, verbose_name=_('Address'), help_text='(*)')
     id_type = models.IntegerField(
         _('ID Type'),
         choices=ID_CHOICES, default=codes.IDType.ID_CARD.value,
         db_index=True,
         help_text="(*)"
     )
-    id_number = models.CharField(max_length=128, db_index=True, verbose_name='ID Number', help_text="(*)")
-    id_card = models.FileField(upload_to=storage.hashfile_upload_to('id_card', path_prefix='id_card'), verbose_name='ID Photo', validators=[validators.validate_file_extension_of_id_photo, validators.validate_file_size_of_id_photo], help_text="(*)")
+    id_number = models.CharField(max_length=128, db_index=True, verbose_name=_('ID Number'), help_text="(*)")
+    id_card = models.FileField(upload_to=storage.hashfile_upload_to('id_card', path_prefix='id_card'), verbose_name=_('ID Photo'), validators=[validators.validate_file_extension_of_id_photo, validators.validate_file_size_of_id_photo], help_text="(*)")
     
     # profile
-    personal_profile = models.TextField(verbose_name="Self Introduction", max_length=10240, help_text="(*)")
-    personal_profile_attachment = models.FileField(upload_to=storage.hashfile_upload_to('personal_profile_attachment', path_prefix='personal_profile_attachment'), verbose_name='Attachment', validators=[validators.validate_file_size_of_id_photo, validators.validate_file_extension_of_id_photo])
+    personal_profile = models.TextField(verbose_name=_("Self Introduction"), max_length=10240, help_text="(*)")
+    personal_profile_attachment = models.FileField(upload_to=storage.hashfile_upload_to('personal_profile_attachment', path_prefix='personal_profile_attachment'), verbose_name=_('Attachment'), validators=[validators.validate_file_size_of_id_photo, validators.validate_file_extension_of_id_photo])
     facebook = models.CharField(max_length=128, db_index=True, verbose_name='Facebook', null=True)
     twitter = models.CharField(max_length=128, db_index=True, verbose_name='Twitter', null=True)
     telegram = models.CharField(max_length=128, db_index=True, verbose_name='Telegram ID', null=True)
-    wechat = models.CharField(max_length=128, db_index=True, verbose_name='WeChat ID', null=True)
-    other_social_account = models.CharField(max_length=128, db_index=True, verbose_name='Other Social Media', null=True)
-    your_community = models.CharField(max_length=1024, verbose_name='Newton Communities You are Involved in', null=True)
-    your_community_screenshots = models.FileField(upload_to=storage.hashfile_upload_to('your_community_screenshots', path_prefix='your_community_screenshots'), verbose_name='Screenshots of Abovementioned Communities')
+    wechat = models.CharField(max_length=128, db_index=True, verbose_name=_('WeChat ID'), null=True)
+    other_social_account = models.CharField(max_length=128, db_index=True, verbose_name=_('Other Social Media'), null=True)
+    your_community = models.CharField(max_length=1024, verbose_name=_('Newton Communities You are Involved in'), null=True)
+    your_community_screenshots = models.FileField(upload_to=storage.hashfile_upload_to('your_community_screenshots', path_prefix='your_community_screenshots'), verbose_name=_('Screenshots of Abovementioned Communities'))
 
     # how to contribute for newton
-    what_is_newton = models.TextField(verbose_name='Tell us your understanding about Newton', max_length=10240, null=True, help_text='(*)')
-    done_for_newton = models.TextField(verbose_name='What contribute you had do for newton', max_length=10240)
-    done_for_newton_attachment = models.FileField(upload_to=storage.hashfile_upload_to('done_for_newton_attachment', path_prefix='done_for_newton_attachment'), verbose_name="Attachment")
-    do_for_newton = models.TextField(verbose_name='What will you do for newton', max_length=10240)
+    what_is_newton = models.TextField(verbose_name=_('Tell us your understanding about Newton'), max_length=10240, null=True, help_text='(*)')
+    done_for_newton = models.TextField(verbose_name=_('What contribute you had do for newton'), max_length=10240)
+    done_for_newton_attachment = models.FileField(upload_to=storage.hashfile_upload_to('done_for_newton_attachment', path_prefix='done_for_newton_attachment'), verbose_name=_("Attachment"))
+    do_for_newton = models.TextField(verbose_name=_('What will you do for newton'), max_length=10240)
     is_establish_node = models.IntegerField(
         _('Do you want to establish a Newton node ?'),
         choices=ESTABLISH_CHOICE, default=codes.EstablishNodeType.YES.value,
@@ -68,17 +68,17 @@ class KYCInfo(models.Model):
             _('Which type of node ?'),
             choices=NODE_CHOICE, default=codes.NodeType.FULL_NODE.value,
         )
-    establish_node_plan = models.TextField(verbose_name='Your plan of node establishment')
+    establish_node_plan = models.TextField(verbose_name=_('Your plan of node establishment'))
 
     # emergency info
-    emergency_contact_first_name = models.CharField(max_length=128, verbose_name='First Name of Emergency Contact', help_text="(*)")
-    emergency_contact_last_name = models.CharField(max_length=128, verbose_name='Last Name of Emergency Contact', help_text="(*)")
-    emergency_contact_country_code = models.CharField(max_length=4, db_index=True, verbose_name='Country Code of Emergency Contact', help_text="(*)")
-    emergency_contact_cellphone = models.CharField(max_length=20, db_index=True, default='', verbose_name='Cellphone of Emergency Contact', help_text="(*)")
-    emergency_country = CountryField(blank_label="Select country or region", verbose_name='Emergency Country or Region', help_text='(*)')
-    emergency_city = models.CharField(max_length=256, verbose_name='City', help_text='(*)')
-    emergency_location = models.CharField(max_length=1024, verbose_name='Emergency Address', help_text='(*)')
-    emergency_relationship = models.CharField(max_length=1024, verbose_name='Emergency Relationship', help_text='(*)')
+    emergency_contact_first_name = models.CharField(max_length=128, verbose_name=_('First Name of Emergency Contact'), help_text="(*)")
+    emergency_contact_last_name = models.CharField(max_length=128, verbose_name=_('Last Name of Emergency Contact'), help_text="(*)")
+    emergency_contact_country_code = models.CharField(max_length=4, db_index=True, verbose_name=_('Country Code of Emergency Contact'), help_text="(*)")
+    emergency_contact_cellphone = models.CharField(max_length=20, db_index=True, default='', verbose_name=_('Cellphone of Emergency Contact'), help_text="(*)")
+    emergency_country = CountryField(blank_label="Select country or region", verbose_name=_('Emergency Country or Region'), help_text='(*)')
+    emergency_city = models.CharField(max_length=256, verbose_name=_('City'), help_text='(*)')
+    emergency_location = models.CharField(max_length=1024, verbose_name=_('Emergency Address'), help_text='(*)')
+    emergency_relationship = models.CharField(max_length=1024, verbose_name=_('Emergency Relationship'), help_text='(*)')
 
     # base fields
     created_at = models.DateTimeField(auto_now_add=True)
