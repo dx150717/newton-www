89cd235ffd529e36a7f530999716cad91f12fd6c
liuchao <cumtbliu@163.com>
2018-06-16 02:28:19 +0800

    expect input middle

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 213
+STATIC_DEFAULT_VERSION = 214
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
