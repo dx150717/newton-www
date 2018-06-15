592e78c46938e9e415bb8bdd475db3f342198bee
Xia Wu <xiawu@newtonproject.org>
2018-06-15 20:23:24 +0800

    fix bug of captcha.js

--- web/web/static/js/captcha.js
+++ web/web/static/js/captcha.js
@@ -2,6 +2,6 @@
  * The implementation of captcha module
  */
 
-$('#id_captcha_tip, #id_captcha_tip').click(function(event){
+$('#id_captcha_image, #id_captcha_tip').click(function(event){
   $('#id_captcha_image').attr('src', '/ishuman/image/?' + Math.random());
 });
