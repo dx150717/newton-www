9d2f9b6b7af189398d3f9165cfe774d9ca3030e9
liuchao <cumtbliu@163.com>
2018-06-19 17:43:24 +0800

    fix urls bug

--- web/web/apps/tokenexchange/urls.py
+++ web/web/apps/tokenexchange/urls.py
@@ -6,8 +6,8 @@ from config import codes
 
 urlpatterns = patterns('',
                        url(r'^$', views.show_tokenexchange_index_view),
-                       url(r'^individual/post/$', views.post_kyc_information, {"kyc_type": codes.INDIVIDUAL.value}),
-                       url(r'^organization/post/$', views.post_kyc_information, {"kyc_type": codes.ORGANIZATION.value}),
+                       url(r'^individual/post/$', views.post_kyc_information, {"kyc_type": codes.KYCType.INDIVIDUAL.value}),
+                       url(r'^organization/post/$', views.post_kyc_information, {"kyc_type": codes.KYCType.ORGANIZATION.value}),
                        url(r'^wait-audit/$', views.show_wait_audit_view),
                        url(r'^invalid-link/$', views.show_invalid_link),
                        url(r'^pending/', views.show_pending_view),
