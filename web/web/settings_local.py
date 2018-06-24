0836b35bd2a1143dbe8dc9661e9463bf92e6b22b
Xia Wu <xiawu@newtonproject.org>
2018-06-24 15:28:40 +0800

    update version number

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 225
+STATIC_DEFAULT_VERSION = 226
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
