965767746263b68644fe436ccec1f2888a5b278f
liuchao <cumtbliu@163.com>
2018-06-27 00:47:08 +0800

    change version

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 232
+STATIC_DEFAULT_VERSION = 233
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
