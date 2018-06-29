892815a0f16a94d90bebf73f117bedb9a5c4daaf
liuchao <cumtbliu@163.com>
2018-06-30 02:10:50 +0800

    for ticket #442

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 241
+STATIC_DEFAULT_VERSION = 242
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
--- web/web/static/js/tokenexchange.js
+++ web/web/static/js/tokenexchange.js
@@ -21,30 +21,34 @@ $("#id_individual_form").submit(function(event){
     errorElement: "div",
     errorClass: "alert alert-danger",
     rules: {
-        first_name: {required: true, minlength: 1, maxlength: 100},
-        last_name: {required: true, minlength: 1, maxlength:100},
+        first_name: {required: true, minlength: 1, maxlength: 32},
+        last_name: {required: true, minlength: 1, maxlength:32},
         id_type: {required: true},
-        id_number: {required: true, minlength:1},
+        id_number: {required: true, minlength:5, maxlength:64},
         id_card: fileRuleWithRequired,
         country: {required: true},
-        city: {required: true},
-        location:{required: true, minlength:1, maxlength:200},
+        city: {required: true, maxlength:64},
+        location:{required: true, minlength:1, maxlength:128},
         cellphone_group_0: {required: true, minlength:1, maxlength:4},
         cellphone_group_1: {required: true, minlength:1, maxlength:20},
+        personal_profile: {maxlength:2048},
         personal_profile_attachment: fileRuleWithoutRequired,
-        your_community_screenshots1: fileRuleWithoutRequired,
-        your_community_screenshots2: fileRuleWithoutRequired,
-        your_community_screenshots3: fileRuleWithoutRequired,
-        how_to_contribute: {required: true, maxlength:1000},
+        telegram: {maxlength: 64},
+        twitter: {maxlength: 64},
+        facebook: {maxlength: 64},
+        wechat: {maxlength: 64},
+        other_social_account: {maxlength: 64},
+        what_is_newton: {maxlength: 2048},
+        done_for_newton: {maxlength:2048},
         done_for_newton_attachment: fileRuleWithoutRequired,
-        what_is_newton: {minlength:1, maxlength:1000},
-        emergency_contact_first_name: {required:true, minlength:1, maxlength:100},
-        emergency_contact_last_name: {required:true, minlength:1, maxlength:100},
+        establish_node_plan: {maxlength: 2048},
+        emergency_contact_first_name: {required:true, minlength:1, maxlength:32},
+        emergency_contact_last_name: {required:true, minlength:1, maxlength:32},
         cellphone_of_emergency_contact_0: {required:true, minlength:1, maxlength:4},
         cellphone_of_emergency_contact_1: {required:true, minlength:1, maxlength:20},
         emergency_country: {required: true},
-        emergency_city: {required: true},
-        emergency_location: {required: true},
+        emergency_city: {required: true, maxlength:64},
+        emergency_location: {required: true, maxlength:128},
         emergency_relationship: {required:true}
     },
     messages: {
@@ -55,7 +59,7 @@ $("#id_individual_form").submit(function(event){
             minlength: $.validator.format( "Please enter at least 1 characters." ),
             maxlength: $.validator.format( "Please enter no more than 100 characters." )},
         country: {required: "This field is required."},
-            id_number: {required: "This field is required.",
+        id_number: {required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." )},
         id_card: {required: "This field is required."},
         cellphone_group_0: {required: "This field is required.",
@@ -112,48 +116,122 @@ $("#id_organization_form").submit(function(event){
     errorElement: "div",
     errorClass: "alert alert-danger",
     rules: {
-        orgnization_name: {required: true},
-        orgnization_code: {required: true},
+        orgnization_name: {
+            required: true, 
+            minlength:1, 
+            maxlength: 128
+        },
+        orgnization_code: {
+            required: true,
+            minlength:1,
+            maxlength: 64
+        },
         orgnization_certificate1: fileRuleWithRequired,
         orgnization_certificate2: fileRuleWithoutRequired,
-        first_name: {required: true, minlength: 1, maxlength: 100},
-        last_name: {required: true, minlength: 1, maxlength:100},
+        first_name: {
+            required: true,
+            minlength: 1,
+            maxlength: 32
+        },
+        last_name: {required: true,
+            minlength: 1,
+            maxlength: 32
+        },
         country: {required: true},
-        cellphone_group_0: {required: true, minlength:1, maxlength:4},
-        cellphone_group_1: {required: true, minlength:1, maxlength:20},
-        personal_profile: {required: true},
+        cellphone_group_0: {
+            required: true,
+            minlength:1,
+            maxlength:4
+        },
+        cellphone_group_1: {
+            required: true,
+            minlength:1,
+            maxlength:20
+        },
+        personal_profile: {
+            required: true,
+            maxlength: 2048
+        },
         personal_profile_attachment: fileRuleWithoutRequired,
-        what_is_newton: {required:true, minlength:1, maxlength:1000},
-        done_for_newton: {required:true},
-        done_for_newton_attachment: fileRuleWithoutRequired
+        twitter: {maxlength: 64},
+        facebook: {maxlength: 64},
+        wechat_platform_name: {maxlength: 64},
+        other_social_account: {maxlength: 64},
+        what_is_newton: {
+            required:true,
+            minlength:1,
+            maxlength:2048
+        },
+        done_for_newton: {
+            required:true,
+            minlength:1,
+            maxlength:2048
+        },
+        done_for_newton_attachment: fileRuleWithoutRequired,
+        establish_node_plan: {maxlength:2048}
     },
     messages: {
-        first_name: {required: "This field is required.",
+        orgnization_name: {
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
-            maxlength: $.validator.format( "Please enter no more than 100 characters." )},
-        last_name: {required: "This field is required.",
+            maxlength: $.validator.format( "Please enter no more than 128 characters." )
+        },
+        orgnization_code: {
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
-            maxlength: $.validator.format( "Please enter no more than 100 characters." )},
-        country: {required: "This field is required."},
-            id_number: {required: "This field is required.",
-            minlength: $.validator.format( "Please enter at least 1 characters." )},
-        id_card: {required: "This field is required."},
-        cellphone_group_0: {required: "This field is required.",
+            maxlength: $.validator.format( "Please enter no more than 128 characters." )
+        },
+        first_name: {
+            required: "This field is required.",
+            minlength: $.validator.format( "Please enter at least 1 characters." ),
+            maxlength: $.validator.format( "Please enter no more than 128 characters." )
+        },
+        last_name: {
+            required: "This field is required.",
+            minlength: $.validator.format( "Please enter at least 1 characters." ),
+            maxlength: $.validator.format( "Please enter no more than 128 characters." )
+        },
+        country: {
+            required: "This field is required."
+        },
+        id_number: {
+            required: "This field is required.",
+            minlength: $.validator.format( "Please enter at least 1 characters." )
+        },
+        cellphone_group_0: {
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
             maxlength: $.validator.format( "Please enter no more than 4 characters." ),
-            number: "Please enter a valid number."},
-        cellphone_group_1: {required: "This field is required.",
+            number: "Please enter a valid number."
+        },
+        cellphone_group_1: {
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
             maxlength: $.validator.format( "Please enter no more than 20 characters." ),
-            number: "Please enter a valid number."},
-        location:{required: "This field is required.",
+            number: "Please enter a valid number."
+        },
+        personal_profile: {
+            required: "This field is required.",
+            maxlength: $.validator.format( "Please enter no more than 2048 characters." )
+        },
+        location:{
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
-            maxlength: $.validator.format( "Please enter no more than 200 characters." )},
-        how_to_contribute: {required: "This field is required.",
-            maxlength: $.validator.format( "Please enter no more than 1000 characters." )},
-        what_is_newton: {required: "This field is required.",
+            maxlength: $.validator.format( "Please enter no more than 200 characters." )
+        },
+        what_is_newton: {
+            required: "This field is required.",
             minlength: $.validator.format( "Please enter at least 1 characters." ),
-            maxlength: $.validator.format( "Please enter no more than 1000 characters." )}
+            maxlength: $.validator.format( "Please enter no more than 2048 characters." )
+        },
+        done_for_newton: {
+            required: "This field is required.",
+            minlength: $.validator.format( "Please enter at least 1 characters." ),
+            maxlength: $.validator.format( "Please enter no more than 2048 characters." )
+        },
+        establish_node_plan: {
+            maxlength: $.validator.format( "Please enter no more than 2048 characters." )
+        }
     },
 
     errorPlacement: function(error,element) {
