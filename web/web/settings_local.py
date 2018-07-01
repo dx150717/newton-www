a4696dbbc08e9a1f46deec90a7e9fbd60996723d
liuchao <cumtbliu@163.com>
2018-07-01 23:04:38 +0800

    fix img alt of Guo Hongchi

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -14,7 +14,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 245
+STATIC_DEFAULT_VERSION = 246
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
