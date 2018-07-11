7e415e7588be19e858e0dd3b600624a6defcbf52
wuwenmin <wuwenmin@diynova.com>
2018-07-11 15:35:35 +0800

    delete web/apps/storage

--- web/web/apps/storage/urls.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
-from . import views
-
-urlpatterns = patterns('',
-                    url(r'^protect/(?P<path>.*)$', views.check_file_permission),
-)
