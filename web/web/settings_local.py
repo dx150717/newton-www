542c2e2f3792a595d500043f999397cf2bf8bbd4
liuchao <cumtbliu@163.com>
2018-07-16 21:29:28 +0800

    add whitepaper page

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 254
+STATIC_DEFAULT_VERSION = 255
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
