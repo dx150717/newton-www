769b268e4f8a74c37fe6153ff9c0a2dd7d872559
liuchao <cumtbliu@163.com>
2018-07-16 22:51:54 +0800

    add background to whitepaper page

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 255
+STATIC_DEFAULT_VERSION = 256
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
