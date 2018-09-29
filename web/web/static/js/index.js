b826bb02643ce371c0118a5e5d155c8dc216f658
wuwenmin <wuwenmin@diynova.com>
2018-09-29 16:19:34 +0800

    for ticket #154

--- web/web/static/js/index.js
+++ web/web/static/js/index.js
@@ -282,26 +282,6 @@ function getErrorMessage(json)
   return json.error_message;
 }
 
-
-window.gotoTop = function(min_height){
-    $("#gotoTop").click(
-        function(){$('html,body').animate({scrollTop:0},700);
-    }).hover(
-        function(){$(this).addClass("hover");},
-        function(){$(this).removeClass("hover");
-    });
-    min_height ? min_height = min_height : min_height = 800;
-    $(window).scroll(function(){
-        var s = $(window).scrollTop();
-        if( s > min_height){
-            $("#gotoTop").fadeIn(100);
-        }else{
-            $("#gotoTop").fadeOut(200);
-        };
-    });
-};
-gotoTop();
-
 $(window).scroll(function(){
 	var height
 	var s = $(window).scrollTop();
