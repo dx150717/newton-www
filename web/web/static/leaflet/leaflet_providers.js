3fa573cce188b635d59d53b9bc9b1b4096819e77
wuwenmin <wuwenmin@diynova.com>
2018-10-20 22:33:30 +0800

    for ticket #187

---
+++ web/web/static/leaflet/leaflet_providers.js
@@ -0,0 +1,30 @@
+L.TileLayer.ChinaProvider = L.TileLayer.extend({
+
+    initialize: function(type, options) { // (type, Object)
+        var providers = L.TileLayer.ChinaProvider.providers;
+
+        var parts = type.split('.');
+
+        var providerName = parts[0];
+        var mapName = parts[1];
+        var mapType = parts[2];
+
+        var url = providers[providerName][mapName][mapType];
+        options.subdomains = providers[providerName].Subdomains;
+
+        L.TileLayer.prototype.initialize.call(this, url, options);
+    }
+});
+
+L.TileLayer.ChinaProvider.providers = {
+    Google: {
+        Normal: {
+            Map: "http://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+        },
+        Subdomains: []
+    }
+};
+
+L.tileLayer.chinaProvider = function(type, options) {
+    return new L.TileLayer.ChinaProvider(type, options);
+};
