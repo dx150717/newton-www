af559d60b6bc2696dcd5516669637a2976b31886
liuchao <cumtbliu@163.com>
2018-10-26 17:00:48 +0800

    fixed bug that webbrowser slide with slide section

--- web/web/static/js/slide-change-section.js
+++ web/web/static/js/slide-change-section.js
@@ -14,7 +14,7 @@ function slideChangeSection(maxIndex, defaultIndex, sectionId){
 		$items.eq(index).siblings(".slider-item").hide();
 	}).mouseout(function(){isStop=false});
 
-	$(sectionId + " .slider-section").on("swipeleft",function(){
+	$(sectionId + " .slider-section .slider-item").on("swipeleft",function(){
 		index++;
 		if(index >= maxIndex) {
 			index=0;
@@ -23,7 +23,7 @@ function slideChangeSection(maxIndex, defaultIndex, sectionId){
 		$items.eq(index).addClass("to-left").show();
 		$items.eq(index).siblings(".slider-item").removeClass("to-left").removeClass("to-right").hide();
 	});
-	$(sectionId + " .slider-section").on("swiperight",function(){
+	$(sectionId + " .slider-section .slider-item").on("swiperight",function(){
 		index--;
 		if (index <= -1) {
 			index = maxIndex - 1;
@@ -46,7 +46,7 @@ function slideIndustry (maxIndex, defaultIndex, sectionId) {
     slideChangeSection(maxIndex, defaultIndex, sectionId);
 }
 
-function slideIndustry (maxIndex, defaultIndex, sectionId) {
+function slideProtocol (maxIndex, defaultIndex, sectionId) {
     slideChangeSection(maxIndex, defaultIndex, sectionId);
 }
 
@@ -56,12 +56,12 @@ function slidePartners (maxIndex, defaultIndex, sectionId) {
 
 $(document).ready(function(e) {
 	var windowWidth = $(window).width();
+    $("a").attr("data-ajax","false");
 	if (windowWidth < 768) {
-	    $("a").attr("data-ajax","false")
         slideFeature(3, 0, "#feature");
         slideNotice(3, 2, "#token-exchange-notice-section");
         slideIndustry(6, 0, "#index-industry");
-        slideIndustry(4, 0, "#protocal-section");
+        slideProtocol(4, 0, "#protocal-section");
         slidePartners(7, 0, "#partners")
 	}
 });
