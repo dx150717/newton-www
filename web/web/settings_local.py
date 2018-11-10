a27d9980c2deac214c7fb74698dbeae0c58cffdc
liuchao <cumtbliu@163.com>
2018-11-10 16:52:05 +0800

    haystack settings

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 339
+STATIC_DEFAULT_VERSION = 340
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
@@ -65,3 +65,7 @@ BTC_MAINNET_EXPLORER = 'https://blockchain.info'
 BTC_TESTNET_EXPLORER = 'https://testnet.blockchain.info'
 ELA_MAINNET_EXPLORER = 'https://blockchain.elastos.org'
 ELA_TESTNET_EXPLORER = 'https://blockchain-beta.elastos.org'
+
+# haystack settings
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 20
