d4efc2e3998140b8e033d7cb96e739e47eecceb4
liuchao <cumtbliu@163.com>
2018-11-10 20:36:07 +0800

    fixed ticket #213

--- web/web/static/js/search-bar.js
+++ web/web/static/js/search-bar.js
@@ -4,6 +4,8 @@ function initSearchBar () {
     var inputStatus = false;
     $("#id_search_icon_container").click(function () {
         if ($(this).children("span").hasClass("glyphicon-search") && inputStatus == false) {
+            $(this).addClass("search_icon_container");
+            $("#id_search_from").addClass("search_form");
             $("#id_search_input").val("");
             $(this).parent("li").siblings().hide();
             $("#id_search_input").fadeIn();
@@ -11,8 +13,10 @@ function initSearchBar () {
             $("#id_search_icon").removeClass("glyphicon-search").addClass("glyphicon-remove");
             inputStatus = true;
         } else if ($(this).children("span").hasClass("glyphicon-remove") && inputStatus == true) {
-            $("#id_search_input").fadeOut();
-            $("#id_search_btn").fadeOut();
+            $(this).removeClass("search_icon_container");
+            $("#id_search_from").removeClass("search_form");
+            $("#id_search_input").hide();
+            $("#id_search_btn").hide();
             $(this).parent("li").siblings().fadeIn();
             $("#id_search_icon").removeClass("glyphicon-remove").addClass("glyphicon-search");
             $("#id_search_input").val("");
