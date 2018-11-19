dffb0883cdaa6e7df4a81696d9e863c065484edf
wuwenmin <wuwenmin@diynova.com>
2018-11-19 22:51:09 +0800

    for ticket #238

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 356
+STATIC_DEFAULT_VERSION = 357
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
