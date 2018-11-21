32fed80ca666e189cb9f9a207099a48b648d9efb
wuwenmin <wuwenmin@diynova.com>
2018-11-21 22:56:41 +0800

    for ticket #219

--- web/web/static/js/events.js
+++ web/web/static/js/events.js
@@ -44,6 +44,33 @@ function initEvents() {
         $("#id_current_month").text(monthChange + " " + year);
         $("#id_events_list_" + year + "_" + monthIndexChange.toString()).show().siblings("div.events-list").hide();
     });
+    $("#id_prev_month_mobile").click(function() {
+        $("#id_coming_events_mobile").removeClass("active");
+        $("#id_passed_events_mobile").removeClass("active");
+        var currentMonthYear = $("#id_current_month_mobile").text().split(" ");
+        var month = currentMonthYear[0];
+        var year = currentMonthYear[1];
+        var monthIndex = monthArray.indexOf(month);
+        var monthIndexChange;
+        if (monthIndex==5 && year=="2018") {
+            return;
+        };
+        if (!(monthIndex==1)) {
+            monthIndexChange = monthIndex - 1;
+        } else {
+            monthIndexChange = 12;
+            monthChange = "December";
+            year = (parseInt(year) - 1).toString();
+        };
+        if (monthIndex==10 && year=="2018") {
+            monthIndexChange = 8;
+        } else if (monthIndex==8 && year=="2018") {
+            monthIndexChange = 6;
+        }
+        monthChange = monthArray[monthIndexChange];
+        $("#id_current_month_mobile").text(monthChange + " " + year);
+        $("#id_events_list_" + year + "_" + monthIndexChange + "_mobile".toString()).show().siblings("div.events-list").hide();
+    });
 
     $("#id_next_month").click(function() {
         $("#id_coming_events").removeClass("active");
@@ -73,12 +100,39 @@ function initEvents() {
         $("#id_current_month").text(monthChange + " " + year);
         $("#id_events_list_" + year + "_" + monthIndexChange.toString()).show().siblings("div.events-list").hide();
     });
+    $("#id_next_month_mobile").click(function() {
+        $("#id_coming_events_mobile").removeClass("active");
+        $("#id_passed_events_mobile").removeClass("active");
+        var currentMonthYear = $("#id_current_month_mobile").text().split(" ");
+        var month = currentMonthYear[0];
+        var year = currentMonthYear[1];
+        var monthIndex = monthArray.indexOf(month);
+        var monthIndexChange;
+        if (monthIndex==12) {
+            return;
+        };
+        if (!(monthIndex==12)) {
+            monthIndexChange = monthIndex + 1;
+        } else {
+            monthIndexChange = 1;
+            monthChange = "January";
+            year = (parseInt(year) + 1).toString();
+        }
+        if (monthIndex==6 && year=="2018") {
+            monthIndexChange = 8;
+        } else if (monthIndex==8 && year=="2018") {
+            monthIndexChange = 10;
+        }
+        monthChange = monthArray[monthIndexChange];
+        $("#id_current_month_mobile").text(monthChange + " " + year);
+        $("#id_events_list_" + year + "_" + monthIndexChange + "_mobile".toString()).show().siblings("div.events-list").hide();
+    });
 
 
     $("#id_passed_events_mobile").click(function() {
         $(this).addClass("active");
-        $("#id_coming_events").removeClass("active");
-        $("#id_events_list_passed").show().siblings("div.events-list").hide();
+        $("#id_coming_events_mobile").removeClass("active");
+        $("#id_events_list_passed_mobile").show().siblings("div.events-list").hide();
     });
 
     $("#id_passed_events").click(function() {
@@ -92,6 +146,13 @@ function initEvents() {
         $("#id_passed_events").removeClass("active");
         $("#id_passed_events_mobile").removeClass("active");
         $("#id_events_list_coming").show().siblings("div.events-list").hide();
+    });
+
+    $("#id_coming_events_mobile").click(function() {
+        $(this).addClass("active");
+        $("#id_passed_events").removeClass("active");
+        $("#id_passed_events_mobile").removeClass("active");
+        $("#id_events_list_coming_mobile").show().siblings("div.events-list").hide();
     });
 };
 
