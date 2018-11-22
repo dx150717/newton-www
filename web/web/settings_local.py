92b30f9a60642dd6c563583f7ff3176290c46db4
wuwenmin <wuwenmin@diynova.com>
2018-11-22 16:36:02 +0800

    change static version

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 361
+STATIC_DEFAULT_VERSION = 362
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
