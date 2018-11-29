a0dc94bb8da596bd13adb7f15d6726bc2d01cc6e
liuchao <cumtbliu@163.com>
2018-11-29 21:35:28 +0800

    add events list page

--- web/web/apps/events/views/__init__.py
+++ web/web/apps/events/views/__init__.py
@@ -1,4 +1,7 @@
 """Views for Zinnia"""
+
+import datetime
+
 from django.views import generic
 from django.utils import translation
 from django.conf import settings
@@ -14,6 +17,20 @@ class EventsView(generic.ListView):
     context_object_name = "entries"
     paginate_by = 20
 
+    def get_context_data(self, **kwargs):
+        context = super(EventsView, self).get_context_data(**kwargs)
+        entries = context["entries"]
+        coming_entries = []
+        passed_entries = []
+        for item in entries:
+            if item.event_date >= datetime.date.today():
+                coming_entries.append(item)
+            else:
+                passed_entries.append(item)
+        context["coming_entries"] = coming_entries[::-1]
+        context["passed_entries"] = passed_entries
+        return context
+
     def get_queryset(self):
         language = translation.get_language()
         language_code = codes.EntryLanguage.ENGLISH.value
@@ -22,7 +39,12 @@ class EventsView(generic.ListView):
                 language_code = language_item[1]
                 break
         entry = EntryDetail()
-        entries = entry.get_queryset().filter(entry_type=TYPE_EVENTS, language=language_code, status=PUBLISHED)
+        entries = entry.get_queryset().filter(entry_type=TYPE_EVENTS, language=language_code, status=PUBLISHED)\
+            .order_by("-event_date")
+        if not entries:
+            entries = entry.get_queryset().filter(entry_type=TYPE_EVENTS,
+                                                  language=codes.EntryLanguage.ENGLISH.value,
+                                                  status=PUBLISHED).order_by("-event_date")
         for entry in entries:
             url = entry.get_absolute_url()
             entry.urls = url
