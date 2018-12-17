6b86916ad6dc6d20d74aee52eb8c9d5ce62604ff
liuchao <cumtbliu@163.com>
2018-12-17 19:35:31 +0800

    fixed ticket #304

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 375
+STATIC_DEFAULT_VERSION = 376
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
