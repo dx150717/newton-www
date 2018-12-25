a797b8154016c964c4b3370910a6e45fa6ec8dd7
weixuefeng <weixuefeng@lubangame.com>
2018-12-26 01:35:28 +0800

    fixed bug for ios

--- web/web/static/js/index-video.js
+++ web/web/static/js/index-video.js
@@ -15,12 +15,30 @@ function initIndexVideos () {
     $("#video-tab-2").click(function () {
         videoPlayer1.pause();
         $("#newton-video-1").hide();
-        $("#newton-video-2").show();
-        $("#newton-video-2_html5_api").show();
-        videojs("newton-video-2", {}, function() {
-            window.videoPlayer2 = this;
-            videoPlayer2.play();
-        });
+        if (!isiOS) {
+            $("#newton-video-2").show();
+            $("#newton-video-2_html5_api").show();
+            videojs("newton-video-2", {}, function() {
+                window.videoPlayer2 = this;
+                videoPlayer2.play();
+            });
+        } else {
+            var video = new tvp.VideoInfo();
+            video.setVid("n0814ugqh49");
+            var player =new tvp.Player();
+            player.create({
+                width:"100%",
+                height:185,
+                video:video,
+                modId:"node-video-tencent2",
+                autoplay:true,
+            });
+            player.onplay = function () {
+                $("#video-tab-1").click(function () {
+                    player.pause();
+                });
+            }
+        }
     });
 };
 
