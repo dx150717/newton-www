9e5afd682e5285cff45c872213f0943d17b1297b
Xia Wu <xiawu@newtonproject.org>
2019-02-18 20:02:08 +0800

    add the download page for sharing invitation code

--- web/web/apps/download/views.py
+++ web/web/apps/download/views.py
@@ -6,7 +6,11 @@ logger = logging.getLogger(__name__)
 
 
 def show_newpay_download_view(request):
-    return render(request, "download/newpay-download.html", locals())
+    code = request.GET.get('code', '')
+    if code:
+        return render(request, "download/newpay-download-invite.html", locals())
+    else:
+        return render(request, "download/newpay-download.html", locals())
 
 def show_newpay_guide_view(request):
     return render(request, "download/newpay-guide.html", locals())
