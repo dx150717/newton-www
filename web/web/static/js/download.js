a5eda8c4e0745eb9f0d8322de5b01141cd8a8f0c
Xia Wu <xiawu@newtonproject.org>
2019-02-18 20:09:09 +0800

    add the image and js for download page

---
+++ web/web/static/js/download.js
@@ -0,0 +1,13 @@
+var u = navigator.userAgent;
+var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+
+if (isAndroid) {
+  $('#id_android_btn').show();
+} else if (isiOS) {
+  $('#id_ios_btn').show();
+} else {
+  $('#id_android_btn,#id_ios_btn').addClass('small-btn');
+  $('#id_android_btn').show();
+  $('#id_ios_btn').show();
+}
