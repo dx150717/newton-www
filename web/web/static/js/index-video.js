72c0955d39d2dce4ce2dee2b215d3ea50587b956
Xia Wu <xiawu@newtonproject.org>
2019-02-25 23:51:05 +0800

    fix bug of ios video

--- web/web/static/js/index-video.js
+++ web/web/static/js/index-video.js
@@ -1,4 +1,29 @@
 /* index page videos js */
+function showAliyunVideoForIOS() {
+  $("#newton-video-2").hide();
+  $("#newton-video-1").hide();
+  $("#node-video-tencent2").show();
+  var video = new tvp.VideoInfo();
+  video.setVid("n0814ugqh49");
+  var tencentPlayer =new tvp.Player();
+  tencentPlayer.create({
+    width:320,
+    height:215,
+    video:video,
+    modId:"node-video-tencent2",
+    autoplay:false,
+    pic:"{% version 'images/meeting/videos/A.jpg' %}",
+  });
+  tencentPlayer.onplay = function () {
+    $("#video-tab-1").click(function () {
+      tencentPlayer.pause();
+      $("#node-video-tencent2").hide();
+      $("#newton-video-2").hide();
+      $("#newton-video-1").show();
+    });
+  }
+}
+
 function initIndexVideos () {
     var videoPlayer1 = videojs('newton-video-1');
     var videoPlayer2 = videojs('newton-video-2');
@@ -23,30 +48,10 @@ function initIndexVideos () {
                 videoPlayer2.play();
             });
         } else {
-            $("#newton-video-2").hide();
-            $("#newton-video-1").hide();
-            $("#node-video-tencent2").show();
-            var video = new tvp.VideoInfo();
-            video.setVid("n0814ugqh49");
-            var tencentPlayer =new tvp.Player();
-            tencentPlayer.create({
-                width:320,
-                height:215,
-                video:video,
-                modId:"node-video-tencent2",
-                autoplay:true,
-                pic:"{% version 'images/meeting/videos/A.jpg' %}",
-            });
-            tencentPlayer.onplay = function () {
-                $("#video-tab-1").click(function () {
-                    tencentPlayer.pause();
-                    $("#node-video-tencent2").hide();
-                    $("#newton-video-2").hide();
-                    $("#newton-video-1").show();
-                });
-            }
+          showAliyunVideoForIOS();
         }
     });
-};
+}
+
 
 initIndexVideos();
