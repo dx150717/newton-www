609ba2bc2ee135bd63ca3c91473e1638f9dbc8b7
Xia Wu <xiawu@newtonproject.org>
2019-02-26 14:01:56 +0800

    add the ios compatible mp4 file

--- web/web/static/js/index-video.js
+++ web/web/static/js/index-video.js
@@ -1,28 +1,4 @@
 /* index page videos js */
-function showAliyunVideoForIOS(autoplay) {
-  $("#newton-video-2").hide();
-  $("#newton-video-1").hide();
-  $("#node-video-tencent2").show();
-  var video = new tvp.VideoInfo();
-  video.setVid("n0814ugqh49");
-  var tencentPlayer =new tvp.Player();
-  tencentPlayer.create({
-    width:320,
-    height:215,
-    video:video,
-    modId:"node-video-tencent2",
-    autoplay:autoplay
-  });
-  tencentPlayer.onplay = function () {
-    $("#video-tab-1").click(function () {
-      tencentPlayer.pause();
-      $("#node-video-tencent2").hide();
-      $("#newton-video-2").hide();
-      $("#newton-video-1").show();
-    });
-  }
-}
-
 function initIndexVideos () {
     var videoPlayer1 = videojs('newton-video-1');
     var videoPlayer2 = videojs('newton-video-2');
@@ -39,18 +15,13 @@ function initIndexVideos () {
     $("#video-tab-2").click(function () {
         videoPlayer1.pause();
         $("#newton-video-1").hide();
-        if (!isiOS) {
-            $("#newton-video-2").show();
-            $("#newton-video-2_html5_api").show();
-            videojs("newton-video-2", {}, function() {
-                window.videoPlayer2 = this;
-                videoPlayer2.play();
-            });
-        } else {
-          showAliyunVideoForIOS(true);
-        }
+        $("#newton-video-2").show();
+        $("#newton-video-2_html5_api").show();
+        videojs("newton-video-2", {}, function() {
+            window.videoPlayer2 = this;
+            videoPlayer2.play();
+        });
     });
-  showAliyunVideoForIOS(false);
 }
 
 
