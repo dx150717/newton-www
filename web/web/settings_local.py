412e5d9e0c351884fd9692ef6db52b9bcdcaa2db
Xia Wu <xiawu@newtonproject.org>
2019-03-02 19:31:34 +0800

    update the css of download

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 387
+STATIC_DEFAULT_VERSION = 388
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
