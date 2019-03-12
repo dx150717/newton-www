ef54d2a757cd52b181b37ee07cc0c0b2d4d2363b
Xia Wu <xiawu@newtonproject.org>
2019-03-12 20:38:07 +0800

    make the previous commit work

--- web/web/apps/download/views.py
+++ web/web/apps/download/views.py
@@ -9,6 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 def show_newpay_download_view(request):
+    is_zh = False
+    language = str(translation.get_language())
+    if language.startswith('zh'):
+        is_zh = True
     code = request.GET.get('code', '')
     if code:
         return render(request, "download/newpay-download-invite.html", locals())
