8589348392c6f497abd0bb8b202e40bcfeaf55d7
zzetian <zzetian@cn.ibm.com>
2019-04-14 22:34:49 +0800

    Suit Mobile

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 398
+STATIC_DEFAULT_VERSION = 399
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
