d1a50b98c1853d6da29747f9e946f17a6074b8e4
tiankai <tiankai@diynova.com>
2019-07-17 16:55:05 +0800

    adjust the image of binance-dex

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 420
+STATIC_DEFAULT_VERSION = 421
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
