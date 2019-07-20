8e121f0b0e7d5b1ff9ccdbb9ca075bfbabc9009b
tiankai <tiankai@diynova.com>
2019-07-20 10:38:12 +0800

    add foundation release this month and change the release address from /contract to /address

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 421
+STATIC_DEFAULT_VERSION = 422
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
