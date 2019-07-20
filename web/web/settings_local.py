a0bfea2a6afb645329f9856e14cc53290e298c33
tiankai <tiankai@diynova.com>
2019-07-20 18:38:56 +0800

    delete

--- web/web/settings_local.py
+++ web/web/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 422
+STATIC_DEFAULT_VERSION = 423
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
